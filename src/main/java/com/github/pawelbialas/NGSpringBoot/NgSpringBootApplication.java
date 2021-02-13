c3b6ec309f3971c4b0a4994d8989679dc4aee9cd
pawel-bialas <zylex@tlen.pl>
2021-02-13 13:17:24 +0100

    first commit

---
+++ src/main/java/com/github/pawelbialas/NGSpringBoot/NgSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.github.pawelbialas.NGSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/pawelbialas/NGSpringBoot/NgSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.pawelbialas.NGSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
