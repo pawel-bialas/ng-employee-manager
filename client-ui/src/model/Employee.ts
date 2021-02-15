53bae2b89f63a4186a0b6e213362b499785896bd
pawel-bialas <zylex@tlen.pl>
2021-02-15 20:04:35 +0100

    reAdded service and model files for the ui

---
+++ client-ui/src/model/Employee.ts
@@ -0,0 +1,11 @@
+export interface Employee {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  jobTitle: string;
+  imageUrl: string;
+  employeeCode: string;
+
+}
---
+++ client-ui/src/service/employee.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Employee} from "../model/Employee";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  private apiServiceUrl = '';
+
+  constructor(private http: HttpClient) { }
+
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>('${this.apiServiceUrl}/employee/findAll');
+  }
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>('${this.apiServiceUrl}/employee/add', employee);
+  }
+  public findEmployee(id: number): Observable<Employee> {
+    return this.http.get<Employee>('${this.apiServiceUrl}/employee/find/' + id);
+  }
+
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>('${this.apiServiceUrl}/employee/update', employee);
+  }
+
+  public deleteEmployee(id: number): Observable<void> {
+    return this.http.delete<void>('${this.apiServiceUrl}/employee/delete/' + id);
+  }
+}
